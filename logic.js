95e03414fd06008c51babe0eda87a40322f4a37f
70m370 <74630076+70m370@users.noreply.github.com>
2024-05-16 17:01:18 -0300

    logic being implemented, im happy to be programing again

--- logic.js
+++ logic.js
@@ -5,16 +5,39 @@ assignment is to do it using the console, i'll do a button to start to play it
 so we dont have a problem while loading the page
 */
 
+
+//global section
 const button = document.querySelector("button");
 const reply = document.querySelector("p");
+
+//functions
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  };
+  
+
+//game logic
 
 function ButtonAction(){
     //receive input
     const UserInp = prompt("ROCK PAPER OR SCISSORS ? ");
 
+    //game logic
+
+    const GameInstruments = ["rock", "paper", "scissors"];
+
+    const Computerchoice = GameInstruments[getRandomInt(3)];
+
+    if(UserInp.toLowerCase() === "rock"){
+            console.log("we got a rock  ");
+        } else {
+            console.log("ain't no rock here");
+        }
+    
+    console.log(`\\n testing stuff: ${Computerchoice}`);    
     //print answer
     reply.textContent = `${UserInp}`;
-}
+};
 
 
 //main
